8360a85dfd08306c578c0a856d01e8702a652d0c
cliff <a0979326987@gmail.com>
2026-07-17 20:52:55 +0800

    refect:update file

--- md_list_generator/md_list_generator.py
+++ md_list_generator/md_list_generator.py
@@ -1,72 +1,70 @@
+from importlib.resources import path
 from pathlib import Path
+from collections import defaultdict
 '''
 放根目錄，抓所有MD名單
 '''
 
 ROOT_FOLDER = Path.cwd()
-#過濾名單
+
+#過濾清單，這些檔案或資料夾不會被列入
 IGNORE_LIST = {'.gitignore', '__pycache__', '.git','README.md'}
 
-class folder_operation:
+class FolderOperation:
+# 資料夾操作，抓取所有md檔案路徑
+
     def __init__(self):
-        pass
-    
+        #路徑定義
+        self.root = ROOT_FOLDER
+        self.output_file = ROOT_FOLDER / "output.txt"
+        self.output_file.parent.mkdir(parents=True, exist_ok=True)
+        
+        #建立存放的字典
+        self.organized_data = defaultdict(list)
+
     def run(self):
         self._get_categories()
 
-    def _should_ignore(self,path):
- 
+    def _get_categories(self):
+        #遍歷資料夾        
 
-        #把路徑拆開過濾
+
+        for path in sorted(self.root.rglob('*.md')):
+
+            #1.判斷是否要忽略
+            if self._should_ignore(path):
+                continue
+            
+            #1-1.取得該路檔案之目錄
+            category = path.parent.name
+            #1-2.取得相對路徑
+            rel_path = path.relative_to(self.root)
+            #1-3.將路徑加入對應的類別中，因為用defaultdict(list)建立，所以不用頭痛key是否存在或重複
+            self.organized_data[category].append(f"- [{path.stem}]({rel_path.as_posix()})")
+
+        self._write_to_file()
+        
+    def _should_ignore(self, path):
+        # 檢查判斷，檔案或資料夾名稱在忽略清單中，或是以 . 開頭的隱藏檔案
         for part in path.parts:
             if part in IGNORE_LIST:
                 return True
-            
-            #模糊匹配
-            if path.name.startswith('.'):
-                return True
-        
 
+        if path.name.startswith('.'):
+            return True
 
-    def _get_categories(self):
-        root = ROOT_FOLDER
-        output_file = ROOT_FOLDER / "output.txt"
-        output_file.parent.mkdir(parents=True, exist_ok=True)
-        for path in sorted(root.rglob('*.md')):
-            
-            if path.name in IGNORE_LIST:
-                continue
-            if self._should_ignore(path):
-                continue
-
-            rel_path = path.relative_to(ROOT_FOLDER)
-            with open(output_file,"a",encoding="utf-8") as f:
-                f.write(str(rel_path) + "\n")
-
-        
-
-class text_editting:
+        return False
     
-    def __init__(self):
-        self.data = ROOT_FOLDER / 'output.txt'
+    def  _write_to_file(self):
+        #將整理好的字典資料輸出為文本
+            output_file = ROOT_FOLDER / "output.txt"
+            with open(output_file, "w", encoding="utf-8") as f:
+                for category in sorted(self.organized_data.keys()):
+                    f.write(f"### {category}\n")
+                    f.write("\n".join(self.organized_data[category]))
+                    f.write("\n\n")
 
-    def format_to_markdown(self):
-
-        structured_data = []
-        with self.data.open(encoding='utf-8') as f :
-            for line in f:
-                clean_path = line.strip() # 刪除換行符  
-                if not clean_path: # split() 處理換行，會產生""，空字串在python是  False
-                    continue
-                p = Path(clean_path)  
-                structured_data.append(f'- [{p.stem}]({p.as_posix()})')
-
-        self.data.write_text("\n".join(structured_data) + "\n", encoding="utf-8")
-        
-        print(f"✅ {self.data.name} 已直接改寫完成！")
 
 if __name__ == "__main__":
-    fo =folder_operation()
-    fo.run()
-    te= text_editting()
-    te.format_to_markdown()
+    folder_interate =FolderOperation().run()
+
