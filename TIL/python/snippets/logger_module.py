6069f3dae91792fc78938a8bbc25834bb9947e49
cliff <a0979326987@gmail.com>
2026-08-07 02:39:33 +0800

    docs:update TIL

--- TIL/python/snippets/logger_module.py
+++ TIL/python/snippets/logger_module.py
@@ -1,17 +1,24 @@
-import src.config as config
+
 import logging
+from config.config_loader import config
+from pathlib import Path
 '''
-功能:日誌模板
+功能:日製模板
+還須設定:config module 、 log path 
+
 '''
 
 def setup_logging():
-    config.LOGGING_PATH.mkdir(parents=True, exist_ok=True)
-
+    log_folder = Path(config.get("logging_setting.log_path"))
+    #mkdir 要的是路徑物件，而不是字串路徑
+    log_folder.mkdir(parents=True, exist_ok=True)
+    log_file_name = config.get("logging_setting.log_file_name")
+    full_log_file_path = log_folder / log_file_name
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s [%(levelname)s] %(message)s',
         handlers=[
             logging.StreamHandler(),
-            logging.FileHandler(config.LOGGING_PATH/"log.log", encoding="utf-8")
+            logging.FileHandler(full_log_file_path, encoding="utf-8")
         ]
     )
